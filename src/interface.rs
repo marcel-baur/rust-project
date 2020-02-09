8583c45dbc633998d0626ea371d01c5163cf6cf0
Marcel Baur <marcel@appdafuer.com>
2020-02-09 07:40:16 +0100

    Some interface.rs Documentation

--- src/interface.rs
+++ src/interface.rs
@@ -39,18 +39,38 @@ pub struct Peer {
     pub redundancy_table: HashMap<SocketAddr, Vec<String>>
 }
 
+/// This function removes the Peer from the Network. Call it if you want to disconnect your
+/// application gracefully while redistributing your locally saved files to the network
+/// # Paramteters
+/// - `peer` - The local `Peer`
 pub fn delete_peer(peer: &mut Peer) {
     send_delete_peer_request(peer)
 }
 
+/// Use this function to control the playback of your music.
+/// # Parameters
+/// - `name` - Name of the file
+/// - `peer` - The local `Peer`
+/// - `state` - The desired `MusicState`
 pub fn music_control(name: Option<String>, peer: &mut Peer, state: MusicState) {
     send_play_request(name, peer, state)
 }
 
+/// Use this function to play, get, order or delete a file
+/// # Parameters
+/// - `peer` - The local `Peer`
+/// - `name` - The name of the file
+/// - `istr` - The desired `Instructions`
 pub fn music_request(peer: &mut Peer, name: &str, instr: Instructions) {
     send_read_request(peer, name, instr)
 }
 
+/// Use this function to upload a file to the network.
+/// # Parameters
+/// - `name` - The name of the file
+/// - `file_path` - The relative path to the file
+/// - `addr` - The local `SocketAddr`
+/// - `peer` - The local `Peer`
 pub fn upload_music(
     name: &str,
     file_path: &str,
@@ -60,6 +80,14 @@ pub fn upload_music(
     push_music_to_database(name, file_path, addr, peer)
 }
 
+
+/// Use this function to connect to the network.
+/// # Parameters
+/// - `module` - A listener object that implements `AppListener` and `Sync` as a boxed value
+/// - `name` - The name by which you want to be represented in the network
+/// - `port` - The port you want to listen on
+/// - `ip` - An optional `SocketAddr`. Pass a value if you want to join a network on that
+///     `SocketAddr`. `None` if you want to start a fresh network.
 pub fn start(module: Box<dyn AppListener + Sync>, name: String, port: String, ip: Option<SocketAddr>) -> Result<Arc<Mutex<Peer>>, String> {
     let clone = Arc::new(Mutex::new(module));
     match network::startup(&name, &port, ip, clone) {
